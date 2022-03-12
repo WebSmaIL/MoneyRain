8c077595d4defb608f9bd131cc4bb25f1555ea6a
Ilya Smakovskiy <smakovskiywebinfo@gmail.com>
2022-03-12 23:54:15 +0600

    first commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "smaweb.guru", //Адрес ftp сервера
+    user: "rc.cun@mail.ru", // Имя пользователя
+    password: "dsd", // Пароль
+    parallel: 5 //Кол-во одновременных потоков
+}
