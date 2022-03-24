e2a8326d3dade199f2cb5656594eeb4b262f2ef8
Ilya Smakovskiy <smakovskiywebinfo@gmail.com>
2022-03-25 02:24:24 +0600

    add burger animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
 import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
+
+const nav = document.querySelector('.header__nav');
+const menu = document.querySelector('.header__flexMenu');
+const burgerBtn = document.querySelector('.header__burger');
+const backBtn = document.querySelector('.header__backBtn');
+
+burgerBtn.addEventListener('click', () => {
+    nav.classList.add('active');
+    menu.classList.add('active');
+})
+
+nav.addEventListener('click', event => {
+    if (event.target === nav || event.target === backBtn) {
+        nav.classList.add('removeAnim');
+        menu.classList.add('removeAnim');
+        setTimeout(() => {
+            nav.classList.remove('removeAnim', 'active');
+            menu.classList.remove('removeAnim', 'active');
+        }, 600)
+    }
+})
